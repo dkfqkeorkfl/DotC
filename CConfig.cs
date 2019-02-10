437ad780cb2a7092734ee1b1fcb855a9343c57a9
Liam Lee <dltmdgud@nate.com>
2019-02-10 19:18:48 +0900

    Config Update

---
+++ CConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DC
+{
+	public static class Config {
+		public const string host_server = "https://localhost:8080";
+		public const float ignore_btn_continuity = 1.0f;
+	}
+}
+
